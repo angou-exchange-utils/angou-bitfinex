bfe1c543a1a2dd3f8f28aed48bab13bada11eb17
shdown <shdownnine@gmail.com>
2018-05-24 19:36:27 +0300

    angou_bitfinex/rest.py: update

--- angou_bitfinex/rest.py
+++ angou_bitfinex/rest.py
@@ -5,6 +5,9 @@ import requests
 from . import auth_utils
 
 
+LOGGER = logging.getLogger('angou_bitfinex')
+
+
 _BASE_URL = 'https://api.bitfinex.com'
 
 
@@ -53,6 +56,10 @@ class _BitfinexV2Auth(requests.auth.AuthBase):
         return r
 
 
+class InvalidJSON(Exception):
+    pass
+
+
 class RestV1Error(Exception):
     pass
 
@@ -64,72 +71,96 @@ class RestV2Error(Exception):
         self.message = message
 
 
-class RestV1Client:
-    def __init__(self, api_key, api_secret):
+class RestV1Session:
+    def __init__(self, api_key, api_secret, timeout=None):
         self.api_key = api_key
         self.api_secret = api_secret
+        self.timeout = timeout
         self._session = requests.Session()
         self._session.headers.update({
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         })
         self._auth = _BitfinexV1Auth(api_key, api_secret)
-        self.logger = logging.getLogger('angou_bitfinex')
 
-    def _postprocess(self, req):
-        r = self._session.send(self._session.prepare_request(req))
+    @staticmethod
+    def _postprocess(r):
         try:
             r.raise_for_status()
         except requests.exceptions.HTTPError:
-            raise RestV1Error(r.json()['message'])
+            try:
+                err_obj = r.json()
+            except ValueError:
+                pass
+            else:
+                try:
+                    raise RestV1Error(err_obj['message'])
+                except KeyError:
+                    pass
+            raise
         else:
-            return r.json()
+            try:
+                return r.json()
+            except ValueError:
+                raise InvalidJSON()
 
     def call_public(self, method, params=None):
-        self.logger.debug('%s %s', method, params)
-        return self._postprocess(requests.Request('GET', f'{_BASE_URL}/v1/{method}', params=params))
+        LOGGER.debug('GET %s %s', method, params)
+        return self._postprocess(self._session.request(
+            'GET', f'{_BASE_URL}/v1/{method}', params=params, timeout=self.timeout))
 
     def call_auth(self, method, params=None):
-        self.logger.debug('%s %s', method, params)
+        LOGGER.debug('POST %s %s', method, params)
 
         path = f'/v1/{method}'
 
+        params = params or {}
         params.update({
             'request': path,
             'nonce': auth_utils.generate_nonce(),
         })
 
-        return self._postprocess(
-            requests.Request('POST', f'{_BASE_URL}{path}', json=params, auth=self._auth))
+        return self._postprocess(self._session.request(
+            'POST', f'{_BASE_URL}{path}', json=params, auth=self._auth, timeout=self.timeout))
 
 
-class RestV2Client:
-    def __init__(self, api_key, api_secret):
+class RestV2Session:
+    def __init__(self, api_key, api_secret, timeout=None):
         self.api_key = api_key
         self.api_secret = api_secret
+        self.timeout = timeout
         self._session = requests.Session()
         self._session.headers.update({
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         })
         self._auth = _BitfinexV2Auth(api_key, api_secret)
-        self.logger = logging.getLogger('angou_bitfinex')
 
-    def _postprocess(self, req):
-        r = self._session.send(self._session.prepare_request(req))
+    @staticmethod
+    def _postprocess(r):
         try:
             r.raise_for_status()
         except requests.exceptions.HTTPError:
-            _, code, message = r.json()
-            raise RestV2Error(code, message)
+            try:
+                err_obj = r.json()
+            except ValueError:
+                pass
+            else:
+                if isinstance(err_obj, list) and len(err_obj) >= 3:
+                    raise RestV2Error(err_obj[1], err_obj[2])
+            raise
         else:
-            return r.json()
+            try:
+                return r.json()
+            except ValueError:
+                raise InvalidJSON()
 
     def call_public(self, method, params=None):
-        self.logger.debug('%s %s', method, params)
-        return self._postprocess(requests.Request('GET', f'{_BASE_URL}/v2/{method}', params=params))
+        LOGGER.debug('GET %s %s', method, params)
+        return self._postprocess(self._session.request(
+            'GET', f'{_BASE_URL}/v2/{method}', params=params, timeout=self.timeout))
 
     def call_auth(self, method, params=None):
-        self.logger.debug('%s %s', method, params)
-        return self._postprocess(
-            requests.Request('POST', f'{_BASE_URL}/v2/{method}', json=params, auth=self._auth))
+        LOGGER.debug('POST %s %s', method, params)
+        return self._postprocess(self._session.request(
+            'POST', f'{_BASE_URL}/v2/{method}', json=params, auth=self._auth, timeout=self.timeout))
